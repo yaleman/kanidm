23259ce75fb6ff15992d88ceb321a261cbb4d557
George Wu <gbw@users.noreply.github.com>
2024-12-30 21:44:45 -0500

    Add support for prefers-color-scheme using Bootstrap classes.

--- server/core/static/pkhtml.js
+++ server/core/static/pkhtml.js
@@ -45,3 +45,23 @@ try {
         asskey_login()
     });
 } catch (_error) {};
+
+function updateColorScheme() {
+    let colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    let invertedColorScheme = colorScheme === 'dark' ? 'light' : 'dark';
+    document.body.setAttribute('data-bs-theme', colorScheme);
+    ["bg"].forEach((cls) => {
+        Array.from(document.getElementsByClassName(`${cls}-${invertedColorScheme}`)).forEach((e) => {
+            if (e.tagName !== "NAV")
+                e.classList.replace(`${cls}-${invertedColorScheme}`, `${cls}-${colorScheme}`);
+        });
+    });
+    ["btn", "link", "text"].forEach((cls) => {
+        Array.from(document.getElementsByClassName(`${cls}-${colorScheme}`)).forEach((e) => {
+            e.classList.replace(`${cls}-${colorScheme}`, `${cls}-${invertedColorScheme}`);
+        });
+    });
+}
+updateColorScheme();
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateColorScheme);
+document.body.addEventListener('htmx:afterOnLoad', updateColorScheme);
