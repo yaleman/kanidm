b2967bdf69954f6032b5821d51c6dfff7007cfd2
James Hodgkinson <james@terminaloutcomes.com>
2026-03-06 10:31:00 +1000

    clippy fix

--- webauthn-authenticator-rs/src/lib.rs
+++ webauthn-authenticator-rs/src/lib.rs
@@ -271,7 +271,7 @@ where
         //      Is not present
         //          Set options.rp.id to effectiveDomain.
 
-        if !(effective_domain == &options.rp.id
+        if !(effective_domain == options.rp.id
             || effective_domain.ends_with(&format!(".{}", &options.rp.id)))
         {
             error!("relying party id domain is not a suffix of the effective domain.");
@@ -334,7 +334,7 @@ where
         //      Is not present
         //          Set options.rp.id to effectiveDomain.
 
-        if !(effective_domain == &options.rp_id
+        if !(effective_domain == options.rp_id
             || effective_domain.ends_with(&format!(".{}", &options.rp_id)))
         {
             error!("relying party id domain is not suffix of effective domain.");
